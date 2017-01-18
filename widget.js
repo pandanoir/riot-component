b9451c55e4a08a695b1243f02dfa1a60ca3caa6e
幾野直人 <moon_in_france@yahoo.co.jp>
2017-01-18 22:02:29 +0900

    added <widget>

---
+++ widget.js
@@ -0,0 +1,2 @@
+riot.tag2('widget', '<yield></yield>', 'widget,[data-is="widget"]{ display: block; border: 1px solid #999; background: #f9f9f9; box-shadow: 0 1px 1px #444; width: 300px; padding: 10px; margin: 6px 0; }', '', function(opts) {
+});
