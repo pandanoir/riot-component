21fbc03c39421956c760c65638c52af5fd9cd89d
pandanoir <moon_in_france@yahoo.co.jp>
2017-01-31 11:44:12 +0900

    updated the design of widget

--- widget.js
+++ widget.js
@@ -1,2 +1,2 @@
-riot.tag2('widget', '<yield></yield>', 'widget,[data-is="widget"]{ display: block; border: 1px solid #999; background: #f9f9f9; box-shadow: 0 1px 1px #444; width: 300px; padding: 10px; margin: 6px 0; }', '', function(opts) {
+riot.tag2('widget', '<yield></yield>', 'widget,[data-is="widget"]{ display: block; background: #f9f9f9; box-shadow: 0 1px 1px #444; width: 300px; padding: 10px; margin: 6px 0; border-radius: 2px; }', '', function(opts) {
 });
