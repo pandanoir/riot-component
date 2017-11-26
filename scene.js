7e24ce3c5e5f9fa7b73c4667f424a33ae5323526
pandanoir <moon_in_france@yahoo.co.jp>
2017-11-26 20:05:11 +0900

    add <scene>

---
+++ scene.js
@@ -0,0 +1,17 @@
+riot.tag2('scene', '<div if="{show}"> <yield></yield> </div>', '', '', function(opts) {
+    if (opts.start) {
+      this.show = false;
+      setTimeout(() => {
+        this.show = true;
+        this.update();
+      }, opts.start);
+    }
+    else this.show=true;
+    console.log(opts);
+    if (opts.timeout) {
+      setTimeout(() => {
+        this.unmount();
+        this.update();
+      }, opts.timeout);
+    }
+});
